9dddaafaafba59284b5d0f1c078aa39acbca4fee
hoseop <dgt2613@naver.com>
2021-11-26 17:50:16 +0900

    step2

--- 17_atsg.tf
+++ 17_atsg.tf
@@ -10,7 +10,7 @@ resource "aws_autoscaling_group" "hoseop_atsg" {
   health_check_grace_period = 60
   health_check_type         = "EC2"
   desired_capacity          = 2
-  force_delete              = true
+  force_delete              = false
   launch_configuration      = aws_launch_configuration.hoseop_lacf.name
   #placement_group           = aws_placement_group.hoseop_pg.id
   vpc_zone_identifier       = [aws_subnet.hoseop_puba.id, aws_subnet.hoseop_pubc.id]
