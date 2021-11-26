e65ce70cd3881b1b62f15ede2f23487ecea1f337
hoseop <dgt2613@naver.com>
2021-11-26 15:47:19 +0900

    step2

--- 15_ami.tf
+++ 15_ami.tf
@@ -1,8 +1,7 @@
 resource "aws_ami_from_instance" "hoseop_ami" {
-  name = "hoseop-ami"
+  name               = "hoseop-ami"
   source_instance_id = aws_instance.hoseop_web.id
   depends_on = [
     aws_instance.hoseop_web
   ]
-
 }
